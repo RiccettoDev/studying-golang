a059b79bcc07f8ce70c9cd8a30c2917df789cb41
Riccetto Dev <riccettodev@gmail.com>
2025-11-04 15:33:09 -0300

    [golang]-feat- cap2 with convertion types

---
+++ aprenda-go/cap2/conversion/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+type hotdog int
+
+var b hotdog = 101
+
+func main() {
+	x := 10
+	fmt.Printf("%v\n", x)
+	// fmt.Printf("%v\n", x)
+
+	x = int(b) // colocando o tipo que vc deseja converter o o valor dentro dele é possivel fazer a conversão
+	// por exemplo aqui colocando b dentro de int() convertemos o tipo de hotdog para int...
+	fmt.Printf("%v\n", x)
+
+	fmt.Println(x == b) // aqui popositalmente estamos deixando assim, pois a ide informa o erro de que os tipos são diferentes nesta operação
+}
